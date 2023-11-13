4e7eeb4d92a21b5c9eadf58307a4b4e228cea2aa
Joshua Hecker <jhecker2001@gmail.com>
2023-11-13 10:32:02 -0600

    Added functionality to retrieve news pertaining to the stock company using News API and to send the news and percentage difference by email if the percentage difference is high enough.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
-# import datetime
+import datetime
+import smtplib
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 ALPHA_VANTAGE_API_KEY = "QRC21Y77Q40OYV1W"
+ALPHA_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_API_KEY = "253da3f883e54107bd488fb78cd853cb"
+NEWS_API_ENDPOINT = "https://newsapi.org/v2/everything"
+SHIFT_AMOUNT = 5
 
 alpha_params = {
     "function": "TIME_SERIES_DAILY",
@@ -14,24 +19,62 @@ alpha_params = {
 # # STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-stock_data = requests.get(url="https://www.alphavantage.co/query", params=alpha_params)
+stock_data = requests.get(url=ALPHA_ENDPOINT, params=alpha_params)
 stock_data.raise_for_status()
 
-stock_json = stock_data.json()["Time Series (Daily)"]
+daily_stock_data = stock_data.json()["Time Series (Daily)"]
 
-yesterdays_date = list(stock_json.keys())[0]
-day_before_yesterday = list(stock_json.keys())[1]
+yesterdays_date = list(daily_stock_data.keys())[0]
+day_before_yesterday = list(daily_stock_data.keys())[1]
 
-yesterday_close = float(stock_json[yesterdays_date]["4. close"])
-dby_close = float(stock_json[day_before_yesterday]["4. close"])
+yesterday_close = float(daily_stock_data[yesterdays_date]["4. close"])
+dby_close = float(daily_stock_data[day_before_yesterday]["4. close"])
 
 percent_change = (yesterday_close - dby_close) / dby_close * 100
 
-print(f"Percent change is {percent_change}%.")
-if percent_change >= 5 or percent_change <= -5:
-    print("Get News")
-else:
-    print("None")
+todays_date = str(datetime.date.today())
+
+news_api_params = {
+    "apiKey": NEWS_API_KEY,
+    "q": f"+\"{COMPANY_NAME}\"",
+    "searchIn": "title",
+    "to": todays_date,
+    "language": "en",
+    "pageSize": 3
+}
+
+if percent_change >= SHIFT_AMOUNT or percent_change <= -SHIFT_AMOUNT:
+    company_news = requests.get(url=NEWS_API_ENDPOINT, params=news_api_params)
+    top_articles = company_news.json()["articles"]
+
+    if percent_change > 0:
+        change_symbol = "+"
+    else:
+        change_symbol = "-"
+
+    message = f"Subject:{STOCK} {change_symbol}{round(abs(percent_change))}%\n\n"
+    for article in top_articles:
+        message += f"""
+Headline: {article['title']}
+By {article['author']} from {article['source']['name']}
+Brief: {article['description']}
+Source: {article['url']}\n\n
+                    """
+
+    message = message.replace("…", "...")
+    message = message.replace("–", "-")
+    sender = "dwdeathwolf@gmail.com"
+    receiver = "jhecker2001@gmail.com"
+    g_pass = "nezinvlcxjckceys"
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=sender, password=g_pass)
+        connection.sendmail(
+            from_addr=sender,
+            to_addrs=receiver,
+            msg=f"{message}"
+        )
 
 # # STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
